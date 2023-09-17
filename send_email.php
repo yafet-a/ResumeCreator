35d45269f200c3f2194ac592e441dcfa1e8d10d9
yafet-a <alulayafet@gmail.com>
2023-09-17 17:32:29 +0000

    security

--- send_email.php
+++ send_email.php
@@ -15,13 +15,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Create a new PHPMailer instance
     $mail = new PHPMailer(true);
 
+    //REMOVING SMTP DETAILS FOR SECURITY REASONS
     try {
         // Server settings
         $mail->isSMTP(); // Set mailer to use SMTP
-        $mail->Host = 'smtp.gmail.com'; // Specify your SMTP server
+        $mail->Host = 'smtp.gmail/outlook/etc.com'; // Specify your SMTP server
         $mail->SMTPAuth = true; // Enable SMTP authentication
-        $mail->Username = 'alulayafet@gmail.com'; // SMTP username
-        $mail->Password = 'cmddcgjaldfgaewh'; // SMTP password
+        $mail->Username = 'example@example.com'; // SMTP username
+        $mail->Password = 'example_password'; // SMTP password
         $mail->SMTPSecure = 'tls'; // Enable TLS encryption, 'ssl' also accepted
         $mail->Port = 587; // TCP port to connect to
 
